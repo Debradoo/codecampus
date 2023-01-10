8a99f997729222f58910d4247b94263f099982b0
Debradoo <doosedear@gmail.com>
2023-01-10 13:16:25 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,21 @@
+console.log("I am asimple application")
+//alert("I am asimple application")
+var y; // variable declaration
+var y = 30; //variable initialisation
+
+
+let welcome =document.getElementById('welcome');
+welcome.innerHTML= "welcome to javascript";
+
+console.log(window);
+/*const nameElmen =document.getElementById("name");
+
+let username = prompt("what is your name?","");
+
+nameElmen.innerHTML= username;*/
+
+const nameElmen =document.querySelector("nameInput");
+const ok = document.querySelector("ok");
+
+let username = prompt("what is your name?","");
+
---
+++ promise.js
@@ -0,0 +1,15 @@
+console.log("I am asimple application")
+//alert("I am asimple application")
+var y; // variable declaration
+var y = 30; //variable initialisation
+
+
+let welcome =document.getElementById('welcome')
+welcome.innerHTML= "welcome to javascript"
+
+console.log(window)
+const nameElmen =document.getElementById("name");
+
+let username = prompt("what is your name?","");
+
+nameElmen.innerHTML= username;
